c2000a754d08c7a756e1e20a856a2b7e60a84b71
Mr.Kadek <kadekyogananda@gmail.com>
2021-04-10 10:21:29 +0700

    123190188

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class Main {
+    public static void main(String[] args) {
+        int angka = 7;
+        char huruf;
+        String kata;
+        int array [] = new int [5];
+        
+  //      String kata_pertama = "ini kata pertama";
+  //      System.out.println("hello world");
+   //     System.out.print("hai\n ");
+   //     System.out.printf("ini angka %d \n", 12);
+        //conditional
+        if (angka==1) {
+            System.out.println("ini angka 1");
+        }else if(angka>=4){
+            System.out.println("ini bukan 1");
+        }
+     
+    }
+}
---
+++ src/mahasiswa.java
@@ -0,0 +1,25 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class mahasiswa {
+    String nama, nim; 
+    int nilai; 
+    
+   // public Mahasiswa (String nama, String nim, int nilai){
+    //    this.nama = nama;
+    }
+    
+    void kuliah (){
+        System.out.println("mahasiswa kuliah");
+    }
+    int hitungNilai (int nilai) {
+        return nilai / 3;
+    }
+}
